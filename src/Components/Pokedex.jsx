4dcb5fdb92a87fded514bb6cfbcda068eb1e2801
Estefania <estefacdn@gmail.com>
2022-05-17 16:10:56 -0500

    Primeros Pasos pokedex

---
+++ src/Components/Pokedex.jsx
@@ -0,0 +1,11 @@
+import React, {useEffect, useState} from 'react'
+
+
+const Pokedex = () => {
+  const [allPokemons, SetAllPokemons] = useState ([]);
+  return (
+    <div>Pokedex</div>
+  )
+}
+
+export default Pokedex
